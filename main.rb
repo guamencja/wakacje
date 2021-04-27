3fc7ab2babd1d4a2e04a66a58529cb6f41d2fa28
Piotr Badełek <piotrbadelek@protonmail.com>
2021-04-27 18:12:55 +0200

    bot jest onlajn

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'discordrb'
 
 # made by @lisqu16, and @programistazpolski
 
-bot = Discordrb::Commands::CommandBot.new token: 'not', prefix: 's!';
+bot = Discordrb::Commands::CommandBot.new token: '', prefix: 's!'; # ej a co gdyby w wersji na githuba,
+# jako token wsadzić link do rickrolla zakodowany w base64 żeby wyglądał jak prawdziwy token?
+# XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 
 #bot.command :ping do |event|
     #m = event.respond "h"
@@ -15,21 +17,23 @@ bot = Discordrb::Commands::CommandBot.new token: 'not', prefix: 's!';
     #nil
 #end
 
-# vacacje
-deadline = Time.new(2021, 06, 25, 13, 30, 0, "+02:00")
-m = bot.send_message "wstaw se id kanalu", "h"
-while Time.now < deadline do 
-    puts Time.now.to_i, deadline.to_i
-    d = deadline.to_i - Time.now.to_i
-    seconds = (d % 60).round
-    minutes = (d % (60 * 60) / 60).round 
-    hours = (d % (60 * 60 * 24) / (60 * 60)).round
-    days = (d / (60 * 60 * 24)).round
-    weeks = (days / 7).round
-    months = (weeks / 4).round
-    m.edit "Pozostało #{days} dni, #{hours} godzin, #{minutes} minut i #{seconds} sekund! :tada:
-To około: #{weeks} tygodni, bądź #{months} miesięcy!"
-    sleep 60
-end
+Thread.new { 
+    # vacacje
+    deadline = Time.new(2021, 06, 25, 13, 30, 0, "+02:00")
+    m = bot.send_message "", "h"
+    while Time.now < deadline do 
+        puts Time.now.to_i, deadline.to_i
+        d = deadline.to_i - Time.now.to_i
+        seconds = (d % 60).round
+        minutes = (d % (60 * 60) / 60).round 
+        hours = (d % (60 * 60 * 24) / (60 * 60)).round
+        days = (d / (60 * 60 * 24)).round
+        weeks = (days / 7).round
+        months = (weeks / 4).round
+        m.edit "Pozostało #{days} dni, #{hours} godzin, #{minutes} minut i #{seconds} sekund! :tada:
+    To około: #{weeks} tygodni, bądź #{months} miesięcy!"
+        sleep 60
+    end
+}
 
 bot.run
